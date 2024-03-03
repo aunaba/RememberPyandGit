050231a5e9d1ee03b0416cb74ccb089d314322b2
= <=>
2024-03-02 20:52:25 -0500

    Metodos listas

---
+++ Metodos listas.py
@@ -0,0 +1,16 @@
+
+
+#prod= ['Manzanas','Peras','Uvas','Moras']
+'''print(f"Porudctos Originales: {prod}")
+prod[0]='Piña'
+print(f"Cambio de primer producto: {prod}")
+'''
+'''newprod=prod.copy()
+#newprod[1]='Melocotón'
+#print(f"Nuevos productos {newprod}, productos cambio 1: {prod}")
+newprod.extend(prod)
+
+
+newprod.append('Aguacate')
+print(prod , newprod)
+'''
