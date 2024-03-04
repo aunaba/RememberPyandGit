8b150922f1aa45fd8195a307e669e908a000f786
= <=>
2024-03-03 19:07:02 -0500

    ejercicio 4 terminado.

--- ejercicios.py
+++ ejercicios.py
@@ -95,3 +95,12 @@ Mostrar los 6 números ganadores de la lotería de forma ordenada.
 import random
 
 
+numeros_loteria=[]
+
+for n in range(0,6):
+    numero=random.randint(1,35)
+    while numero in numeros_loteria:
+        numero=random.randint(1,35)
+    numeros_loteria.append(numero)
+
+print("Los números de la lotería ganadores son: " , numeros_loteria)
