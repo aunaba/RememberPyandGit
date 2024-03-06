a03e412d825608051eca6bd35bc45035a0bf9763
Juan Tabares <juanjose96t@gmail.com>
2024-03-05 20:10:50 -0500

    versión 1

---
+++ funcionesAnidadas.py
@@ -0,0 +1,11 @@
+#manejo de funciones anidadas:
+def primera(datos):
+    print(datos)
+    
+    def segunda():
+        for n in datos:
+            print(n)
+
+    segunda()
+
+primera([2,4,6,8]) 
