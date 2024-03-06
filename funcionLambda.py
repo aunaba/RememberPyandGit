50f6256fe06f9b0577fe394d2013957ba731d512
Juan Tabares <juanjose96t@gmail.com>
2024-03-05 20:34:57 -0500

    Función lambda explicada

---
+++ funcionLambda.py
@@ -0,0 +1,9 @@
+#Ayuda a resumir a un código o a una función más específica:
+
+'''def potencia(numero, exponente):
+    return pow(numero, exponente)
+'''
+potencia= lambda num, exp : pow(num,exp)
+
+print(potencia(5,2))
+
