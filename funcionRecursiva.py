587ec07b9728bf97fb1ec9502e0bce559706a5b5
Juan Tabares <juanjose96t@gmail.com>
2024-03-05 20:27:28 -0500

    Versión 1

---
+++ funcionRecursiva.py
@@ -0,0 +1,21 @@
+#Comunmente se usa en funciones factoriales
+
+def factorial(numero):
+    if numero > 1:
+        numero = numero * factorial(numero-1)
+    return numero
+
+#numero=int(input('Ingrese número: '))
+#print(factorial(numero))
+
+def contador_atras(numero):
+    numero-=1
+    if(numero>0):
+        print(numero)
+        contador_atras(numero)
+    else():
+        print("Fin")
+    
+    print(f"Liberación: {numero}")
+    
+contador_atras(5)
