f740a02cdba7a3502cabf72e001391375433f23d
Juan Tabares <juanjose96t@gmail.com>
2024-03-07 15:48:03 -0500

    versión 2 main

---
+++ POO/main.py
@@ -0,0 +1,26 @@
+# MÓDULOS
+from poo import Class_Persona
+
+#Instancia de la clase Class_Persona() en el archivo poo.py
+per1=Class_Persona('Juan',27,323123456)
+per2=Class_Persona('Carol', 23,3001234567)
+per3=Class_Persona('Noa',5,30054321)
+
+'''
+#Devolver los datos
+per1.getDatos()
+print('*'*10)
+per2.getDatos()
+print('*'*10)
+per3.getDatos()'''
+
+'''#la función type(), devuelve el tipo de dato
+print(type(None))
+print(type(True))
+print(type(5))
+print(type(5.5))
+print(type('Hola'))
+print(type([]))
+print(type(()))
+print(type({}))
+'''
